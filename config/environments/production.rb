296d615db8220568afe9e8e9ce238a73e5808c2c
Oliver <s3781403@student.rmit.edu.au>
2021-05-24 18:45:13 +1000

    add heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,11 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
+
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
